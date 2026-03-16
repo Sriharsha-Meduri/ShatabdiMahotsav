b7ae92b338493c68f8aa748733da40384b22e5a0
Hema Chandra <150141938+hemachandra0070@users.noreply.github.com>
2026-03-16 17:54:14 +0530

    Changed venue of Classical music concert

--- src/data/eventsCatalog.ts
+++ src/data/eventsCatalog.ts
@@ -31,7 +31,7 @@ export const EVENTS: EventData[] = [
     month: "March 2026",
     title: "Classical Music Concert",
     convenor: "Prof. K. Saraswathi Vidyardhi, HoD Music",
-    venue: "Y.V.S. Murthy Auditorium",
+    venue: "AU Convention Centre",
     description: "An evening of serene classical melodies celebrating our cultural heritage and musical legacy.",
     brochureLink: "/downloads/17 Mar - Classical Music Concert - Poster.pdf"
   },
